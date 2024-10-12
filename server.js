67bcdcb561900cc4297965ee11411968daae8da0
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-10-12 16:22:42 +0530

    cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import router from "./routes/router.js";
 import bodyParser from "body-parser";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+app.use(cors({
+    origin: ['https://snippets-fornt.netlify.app', 'https://snippets-frontend-beta.vercel.app']
+}))
+
 app.get("/api", (req, res)=>{
     console.log("Hii This is a Backend Call");
     return res.json("Hii This is a Backend Call");
