141378557b51733306a146e5556e7e0945bf2cf0
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-10-12 22:57:27 +0530

    forntend added in cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ["http://localhost:4173", "http://localhost:5173"],
+    origin: ["http://localhost:4173", "http://localhost:5173", "https://snippets-frontend-beta.vercel.app"],
     credentials: true
 }))
 
